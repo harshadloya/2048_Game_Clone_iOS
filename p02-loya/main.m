0ccf0015c07632b9a0dff56ae74f6f33e67aeae6
Harshad Loya <harshadloya@Harshads-iMac.local>
2017-02-05 11:03:51 -0800

    Initial Commit

---
+++ p02-loya/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  p02-loya
+//
+//  Created by Harshad Loya on 2/5/17.
+//  Copyright © 2017 Harshad Loya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
